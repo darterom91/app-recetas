cd281ba3eb201b03aa11574b619764754ec3f967
David Artero <d.arterom@gmail.com>
2019-10-29 21:19:39 +0100

    add headre, footer and card with textarea

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <CHeader/>
+    <CSection/>
+    <CFooter/>
   </div>
 </template>
+
+<script>
+import CHeader from '@/components/CHeader.vue';
+import CSection from '@/components/CSection.vue';
+import CFooter from '@/components/CFooter.vue';
+export default {
+  components: {
+    CHeader,
+    CSection,
+    CFooter
+  },
+}
+</script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    uri: 'https://api.edamam.com/api/nutrition-data?app_id=ea848d10&app_key=a52dffc48288ee6c01a89439c7a26aa1&ingr=',
     ingr: ''
   },
   mutations: {
+    getIngr(){
+
+    },
+    // selvPersonInfo(state) {
+    //   console.log("selvPersonInfo");
+
+    //   state.vPersonInfo = true;
+    //   state.vInfoOther = false;
+    //   state.vSkills = false;
+    //   state.vProyectos = false;
+
+    //   console.log("vPersonInfo = " + state.vPersonInfo + "\n" +
+    //     "vInfoOther = " + state.vInfoOther + "\n" +
+    //     "vSkills = " + state.vSkills + "\n" +
+    //     "vPersonInfo = " + state.vPersonInfo);
+    // }
   },
   actions: {
+    
   },
   modules: {
   }
