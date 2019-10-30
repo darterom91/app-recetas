4a9695d99bfe4ff4aa9819d893631668bf736064
David Artero <d.arterom@gmail.com>
2019-10-30 20:07:06 +0100

    add axio in header, delete in stored and call ingr for query

--- src/components/CSection.vue
+++ src/components/CSection.vue
@@ -6,7 +6,7 @@
           <div class="card">
             <div class="card-body">
               <h2 class="card-title">Introduce los ingredientes</h2>
-              <form action="">
+              <form @submit.prevent="loadData">
                 <div class="form-group">
                   <label for="exampleFormControlTextarea1">Cantidad ingrediente(2 tomatos)</label>
                   <textarea v-model="ingredient" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
@@ -24,10 +24,47 @@
 </template>
 
 <script>
+// axios({
+//         method: 'post', //you can set what request you want to be
+//         url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'x-app-id': '5b67f155',
+//           'x-app-key': '6cc07658e30ebd15559daa318cf5567f',
+//           'x-remote-user-id': 0
+//         }, 
+//         body: {
+//           "query": "900 tomatoes"
+//         }
+import axios from 'axios';
 export default {
   data() {
     return {
       ingredient:'',
+      nutrition: [],
+    }
+  },
+  methods: {
+    loadData(){
+      const params = {
+              'query': '2 tomatoes',
+              
+    };
+    console.log("this.ingredient");
+    console.log(this.ingredient);
+      axios.get('https://api.edamam.com/api/nutrition-data?app_id=ea848d10&app_key=a52dffc48288ee6c01a89439c7a26aa1',{
+        params: {
+          'ingr': this.ingredient
+        }
+      }).then(response =>{
+        console.log("response");
+        console.log(response);
+        this.nutrition = response.data;
+        console.log("nutrition");
+        console.log(this.nutrition);
+      }).catch(e => {
+        console.log(e)
+      })
     }
   },
 }
